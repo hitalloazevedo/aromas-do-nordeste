298ffa627f14a5e26bb6c72bd9b1aeb9164ed05c
Hitallo Azevedo <azevedohitallo@gmail.com>
2024-11-25 19:07:27 -0300

    Updates

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './globals.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router}/>
-)
